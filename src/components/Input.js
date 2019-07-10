0302593d105b17430c1bd0d417fb342fe2b51c5f
Metrior <warshavskiythebest@gmail.com>
2019-07-10 15:01:08 +0300

    innitial commit

---
+++ src/components/Input.js
@@ -0,0 +1,59 @@
+import React, {Component, memo} from "react"
+import axios from 'axios';
+
+import {ObjectInspector} from 'react-inspector';
+
+class Input extends Component {
+    constructor(){
+    super();
+        this.state = {
+            url: "",
+            obj: "",
+        }
+    };
+
+    handleClick = () => {
+        axios
+            .get(this.state.url)
+            .catch(err=>console.log(err))
+            .then(response=>this.setState({obj: response.data}))
+    };
+
+    onChangeHandle = (e) => {
+        return this.setState({url: e.target.value})
+    };
+
+    // jsonView = () => {
+    //     let jsonViewer = new JSONViewer();
+    //
+    // };
+
+    // setJSON = () => {
+    //     let jsonObj = {};
+    //     try {
+    //         let value = this.state.obj.value;
+    //         jsonObj = JSON.parse(value);
+    //     }
+    //     catch (err) {
+    //         alert(err);
+    //     }
+    // };
+
+    render() {
+        const { obj } = this.state;
+        return (
+            <div>
+                {"JSON URL"}
+                <input type="text" onChange={this.onChangeHandle}/>
+                <input
+                    type="button"
+                    value="Show JSON"
+                    onClick={this.handleClick}
+                />
+                <ObjectInspector data={obj} />
+            </div>
+        );
+    };
+};
+
+export default memo(Input);
